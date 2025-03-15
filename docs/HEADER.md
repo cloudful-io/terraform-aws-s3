cf803f184ab643cc3a836d54c54c98f057c2dc27
Will Yip <will@cloudful.io>
2025-03-15 17:45:09 -0400

    Moved comments into HEADER.md

---
+++ docs/HEADER.md
@@ -0,0 +1,16 @@
+ # terraform-aws-s3
+ This Terraform module is used to instantiate an AWS S3 Bucket based on [AWS Security Best Practices](https://docs.aws.amazon.com/AmazonS3/latest/userguide/security-best-practices.html):
+ - Enforce encryption of data at rest using AWS Key Management Service (KMS)
+ - Enforce encryption of data in transit
+ - Consider using S3 Object Lock
+ - Implement least privilege access by blocking public access by default
+ - Enforce S3 Versioning
+ - Enable Amazon S3 server access logging
+ ## Usage
+ To instantiate an AWS S3 bucket based on the AWS Security Best Practices, you simply need to provide a unique bucket name.  Bucket names must be unique across all AWS accounts in all the AWS Regions within a partition. A partition is a grouping of Regions. Since server access logging is enabled by default, you must either specify the name of an existing S3 bucket where the logs will be stored, or specify to have a new bucket created by setting `create_logging_bucket` to `true`.  If the S3 bucket is intended to be used for static website hosting, the following will happen:
+ - Encryption of data at rest will use Amazon S3 Managed Keys (SSE-S3)
+ - Encryption of data in transit will not be enforced
+ - Public access will be enabled
+ - A bucket policy will be created to allow anyone to perform `GetObject` on any S3 objects
+ ## Diagram
+ ![Diagram](./diagram.png)
--- main.tf
+++ main.tf
@@ -1,23 +1,3 @@
-/**
- * # terraform-aws-s3
- *
- * This Terraform module is used to instantiate an AWS S3 Bucket based on [AWS Security Best Practices](https://docs.aws.amazon.com/AmazonS3/latest/userguide/security-best-practices.html):
- * - Enforce encryption of data at rest using AWS Key Management Service (KMS)
- * - Enforce encryption of data in transit
- * - Consider using S3 Object Lock
- * - Implement least privilege access by blocking public access by default
- * - Enforce S3 Versioning
- * - Enable Amazon S3 server access logging
- * ## Usage
- * To instantiate an AWS S3 bucket based on the AWS Security Best Practices, you simply need to provide a unique bucket name.  Bucket names must be unique across all AWS accounts in all the AWS Regions within a partition. A partition is a grouping of Regions. Since server access logging is enabled by default, you must either specify the name of an existing S3 bucket where the logs will be stored, or specify to have a new bucket created by setting `create_logging_bucket` to `true`.  If the S3 bucket is intended to be used for static website hosting, the following will happen:
- * - Encryption of data at rest will use Amazon S3 Managed Keys (SSE-S3)
- * - Encryption of data in transit will not be enforced
- * - Public access will be enabled
- * - A bucket policy will be created to allow anyone to perform `GetObject` on any S3 objects
- * ## Diagram
- * ![Diagram](./diagram.png)
- */
-
 resource "aws_s3_bucket" "logging_bucket" {
   count = var.create_logging_bucket ? 1 : 0
   bucket = var.logging_bucket_name
@@ -43,7 +23,6 @@ resource "aws_s3_bucket_policy" "logging_bucket_policy" {
 resource "aws_s3_bucket" "secure_bucket" {
   bucket = var.bucket_name
 }
-
 
 resource "aws_s3_bucket_website_configuration" "s3_website_configuration" {
   count  = var.static_website_hosting ? 1 : 0
