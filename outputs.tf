0dcb5d88a4e95930d4dd56505c6ba0ca04fc7f9a
Will Yip <will@cloudful.io>
2025-03-15 20:52:48 -0400

    Test commit

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,7 @@ output "bucket_name" {
   value       = aws_s3_bucket.secure_bucket.id
 }
 
-output "region" {
+output "region" { 
   description = "The region of the created S3 bucket."
   value       = aws_s3_bucket.secure_bucket.region
 } 
